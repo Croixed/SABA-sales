5c8e657d0ef616cc0f44ef47a2bbe8d04c8f3538
me <a@a.a>
2022-06-28 08:31:57 -0700

    finish basic form handler

---
+++ js/product-form.js
@@ -0,0 +1,16 @@
+import CompaniesController from "./productsController.js";
+
+const companiesController = new CompaniesController();
+
+const form = document.querySelector("#main-form");
+
+form.onsubmit = e => {
+  e.preventDefault();
+  const name = e.target.querySelector("#product-title").value;
+  const desc = e.target.querySelector("#description").value;
+  const imgUrl = e.target.querySelector("#image-url").value;
+  const createdAt = new Date();
+  companiesController.addCompany(name, desc, imgUrl, createdAt);
+
+  e.target.reset(); // resets input fields
+}
