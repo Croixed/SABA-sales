c57db2f213cae4532aaf5369fec98a3bda2f2565
me <a@a.a>
2022-06-28 06:24:51 -0700

    finished task 4

---
+++ js/productsController.js
@@ -0,0 +1,27 @@
+
+class CompaniesController {
+
+  constructor(currentId=0) {
+    this.companies = [];
+    this.currentId = currentId;
+  }
+
+  addCompany(name, desc, imgUrl, createdAt) {
+    let company = {
+      name: name, 
+      desc: desc,
+      imgUrl: imgUrl,
+      createdAt: createdAt,
+      id: this.currentId++,
+    }
+
+    this.companies.push(company);
+  }
+}
+
+const companyController = new CompaniesController();
+
+companyController.addCompany('juicer', 'lorem ipsum', 'some url', Date())
+companyController.addCompany('foodstuffs', 'ipsum ipsum ipsum', 'some url 2', Date())
+
+console.log(companyController.companies);
