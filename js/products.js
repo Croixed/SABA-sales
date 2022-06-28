6c37e08ed2b4cbbce40472a5957f9dc9e6589fd8
me <a@a.a>
2022-06-28 07:22:07 -0700

    local storage saving works

--- js/products.js
+++ js/products.js
@@ -2,17 +2,11 @@ import CompaniesController from "./productsController.js";
 
 const companyController = new CompaniesController();
 
-companyController.addCompany('juicer', 'lorem ipsum', 'some url', Date())
-companyController.addCompany('foodstuffs', 'ipsum ipsum ipsum', 'some url 2', Date())
-
 
 const productsContainer = document.querySelector('#list-companies');
-
 const sampleCard = document.querySelector('.card')
-console.log(sampleCard.querySelector('img'));
 
-
-const addProductCard2 = company => {
+const addProductCard = company => {
   const clonedCard = sampleCard.cloneNode(true);
   clonedCard.querySelector('img').src = './img/lemonade.png'; // company.imgUrl; hardcoding for testing
   clonedCard.querySelector('h5').innerText = company.name;
@@ -21,9 +15,25 @@ const addProductCard2 = company => {
   productsContainer.appendChild(clonedCard);
 }
 
-addProductCard2(companyController.companies[0]);
-addProductCard2(companyController.companies[1]);
+
+// samples
+// companyController.addCompany('juicer', 'lorem ipsum', 'some url', Date())
+//companyController.addCompany('foodstuffs', 'ipsum ipsum ipsum', 'some url 2', Date())
+
+// adding samples to DOM
+// addProductCard(companyController.companies[0]);
+// addProductCard(companyController.companies[1]);
 
 
 
+const loadLocalSamples = () => {
+  if (!localStorage.getItem('companies')) {
+    const sampleController = new CompaniesController();
+    sampleController.addCompany('juicer', 'lorem ipsum', 'some url', Date())
+    sampleController.addCompany('foodstuffs', 'ipsum ipsum ipsum', 'some url 2', Date())
+    localStorage.setItem('companies', JSON.stringify(sampleController.companies));
+  }
+  
+}
 
+loadLocalSamples();
