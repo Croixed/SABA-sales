542e32ac849e2bb07bea4214978f1c5f60423093
me <a@a.a>
2022-08-04 11:34:16 -0700

    slightly optimized product cards

--- js/products.js
+++ js/products.js
@@ -8,9 +8,10 @@ const sampleCard = document.querySelector('.card')
 
 const addProductCard = company => {
   const clonedCard = sampleCard.cloneNode(true);
-  clonedCard.querySelector('img').src = './img/lemonade.png'; // company.imgUrl; hardcoding for testing
+  //clonedCard.querySelector('img').src = './img/lemonade.png'; // company.imgUrl; hardcoding for testing
+  clonedCard.querySelector('img').src = company.imageUrl;
   clonedCard.querySelector('h5').textContent = company.name;
-  clonedCard.querySelector('.card-text').textContent = company.desc;
+  clonedCard.querySelector('.card-text').textContent = company.description;
 
   productsContainer.appendChild(clonedCard);
 }
@@ -19,13 +20,14 @@ const addProductCard = company => {
 const loadLocalSamples = () => {
   if (!localStorage.getItem('companies')) {
     const sampleController = new CompaniesController();
-    sampleController.addCompany('juicer', 'lorem ipsum', 'some url', Date())
-    sampleController.addCompany('foodstuffs', 'ipsum ipsum ipsum', 'some url 2', Date())
-    sampleController.addCompany('coffee', 'words words words words', 'some url 3', Date())
+    sampleController.addCompany('juicer', 'lorem ipsum', 'https://i.imgur.com/AR31QH3.png', Date())
+    sampleController.addCompany('foodstuffs', 'ipsum ipsum ipsum', 'https://i.imgur.com/iFo5o3E.png', Date())
+    // sampleController.addCompany('coffee', 'words words words words', 'some url 3', Date())
     localStorage.setItem('companies', JSON.stringify(sampleController.companies));
   }
 }
 
+console.log(companiesController.companies);
 const renderCardsFromController = () => {
   companiesController.companies.forEach(company => addProductCard(company));
 }
